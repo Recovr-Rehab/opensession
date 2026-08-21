c195400db23df3a83f5f0c1d9d306da7f84b6adf
Kent de Bruin <hi@kentdebruin.com>
2026-08-21 05:41:03 +0000

    Increase iOS Next button spacing

--- packages/clients/ios/OS1/Views/SessionView.swift
+++ packages/clients/ios/OS1/Views/SessionView.swift
@@ -2667,7 +2667,7 @@ private struct SessionInputBar: View {
                     }
                     if let onNextChat {
                         nextChatButton(action: onNextChat)
-                            .padding(.trailing, 8)
+                            .padding(.trailing, 12)
                     }
                 }
             }
